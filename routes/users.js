6491f33a302075e66c81f48bebcc1e8845c405d4
Jimmy ChangWenChi <geograp232@gmail.com>
2022-04-26 22:50:03 +0800

    modify route of user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,90 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
-
+const User = require("../models/user")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//GET user
+router.get("/",(req,res)=>{
+  const Alldata = await User.find();
+  res.status(200).json({
+    "status":"success",
+    Alldata
+  })
+})
+
+
+// POST user
+router.post("/", function(req,res){
+  try{
+    const data = req.body;
+    let nickName = data.nickName;
+    let gender = data.gender;
+    let avatar = data.avatar;
+    if(nickName !== undefined || gender !== undefined){
+      const newUser = await User.create({nickName,gender,avatar});
+      res.status(200).json({
+        "status":"success",
+        "message":"update done"
+      })
+    }else{
+      res.status(400).json({
+        "status":"false",
+        "message":"請確認欄位是否正確"
+      })
+    }
+  }catch(err){
+    res.status(400).json({
+      "status":"false",
+      "message":err
+    })
+  }
+})
+
+// PATCH user
+router.patch("/:id",(req,res)=>{
+  try{
+    const id = req.params.id;
+    const data = req.body;
+    let nickName = data.nickName;
+    let gender = data.gender;
+    let avatar = data.avatar;
+
+    if(nickName !== undefined || gender !== undefined){
+      const newUser = await User.findByIdAndUpdate(id,req.body);
+      res.status(200).json({
+        "status":"success",
+        "message":"update done"
+      })
+    }else{
+      res.status(400).json({
+        "status":"false",
+        "message":"id 不存在"
+      })
+    }
+
+
+  }catch(err){
+    res.status(400).json({
+      "status":"false",
+      "message":err
+    })
+  }
+})
+
+//delete User
+router.delete("/",(req,res)=>{
+  await User.deleteMany({})
+  const AllUsers = await User.find()
+  res.status(200).json({
+    "status":"success",
+    "message":"delete done",
+    "data":AllUsers
+  })
+})
+
 
 module.exports = router;
